29cb49d38f854fd3e79c72e2f46d61a76c606b3e
Alok Kumar <96782177+OfficialAlok@users.noreply.github.com>
2024-11-16 13:50:43 +0530

    Update sea_level_predictor.py

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -13,13 +13,13 @@ def draw_plot():
     # Create first line of best fit
     res1 = linregress(df["Year"], df["CSIRO Adjusted Sea Level"])
     year = range(df["Year"][0], 2050)
-    plt.plot(year, res1.intercept + res1.slope*year, color="green")
+    plt.plot(year, res1.intercept + res1.slope*year, color="red")
 
     # Create second line of best fit
     df2 = df[df["Year"] >= 2000]
     res2 = linregress(df2["Year"], df2["CSIRO Adjusted Sea Level"])
     y = range(2000, 2051)
-    plt.plot(y, res2.intercept + res2.slope*y, color = "red")
+    plt.plot(y, res2.intercept + res2.slope*y, color = "green")
 
     # Add labels and title
     plt.xlabel("Year")
